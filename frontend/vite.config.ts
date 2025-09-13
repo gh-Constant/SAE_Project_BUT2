a50412b9f6486a2f3a87d5cef20c8b9f15800c31
Constant <constantsuchet@gmail.com>
2025-09-13 11:29:53 +0200

    Added github CLI to deploy standalone frontend
    Also added the .env variable that we will use to deploy the frontend without using the backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,41 +1,47 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
-export default defineConfig(() => ({
-  root: __dirname,
-  cacheDir: '../node_modules/.vite/frontend',
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-  plugins: [vue()],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-  build: {
-    outDir: './dist',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+export default defineConfig(({ mode }) => {
+  // Charger les variables d'environnement
+  const env = loadEnv(mode, process.cwd());
+  const noBackend = env.VITE_NO_BACKEND === 'true';
+
+  return {
+    root: __dirname,
+    cacheDir: '../node_modules/.vite/frontend',
+    server: {
+      port: 4200,
+      host: 'localhost',
     },
-  },
-  test: {
-    name: '@sae-project-but2/frontend',
-    watch: false,
-    globals: true,
-    environment: 'jsdom',
-    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: './test-output/vitest/coverage',
-      provider: 'v8' as const,
+    preview: {
+      port: 4300,
+      host: 'localhost',
     },
-  },
-}));
+    plugins: [vue()],
+    build: {
+      outDir: path.resolve(__dirname, './dist'),
+      emptyOutDir: true,
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+    },
+    define: {
+      __NO_BACKEND__: noBackend,
+    },
+    test: {
+      name: '@sae-project-but2/frontend',
+      watch: false,
+      globals: true,
+      environment: 'jsdom',
+      include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      reporters: ['default'],
+      coverage: {
+        reportsDirectory: './test-output/vitest/coverage',
+        provider: 'v8' as const,
+      },
+    },
+  };
+});
