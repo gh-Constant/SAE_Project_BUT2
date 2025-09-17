8a84445096adacf1d4ce20a41806bb49c01f2393
Constant <constantsuchet@gmail.com>
2025-09-17 14:27:17 +0200

    fix backend deployment 1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 
-const host = process.env.HOST ?? 'localhost';
+const host = process.env.HOST ?? '0.0.0.0';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
