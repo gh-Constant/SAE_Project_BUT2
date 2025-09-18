148cf5611fd273fb9b75e8bca555d815bbaea566
Constant <constantsuchet@gmail.com>
2025-09-18 15:49:50 +0200

    fixed lint issues frontend

--- frontend/src/services/mock/apiService.ts
+++ frontend/src/services/mock/apiService.ts
@@ -1,7 +1,7 @@
 import { USERS } from '@/mocks';
 
 export const apiService = {
-  login(email, password) {
+  login(email : string, password : string) {
     return new Promise((resolve, reject) => {
       const user = USERS.find(u => u.email === email && u.password_hashed === password);
       if (user) resolve(user); // if resolved login return the user table
