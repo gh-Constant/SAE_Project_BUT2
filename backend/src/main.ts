d55dadb444fa7c06fb3d0576a9b43eb8fde8ab35
Constant <constantsuchet@gmail.com>
2025-09-21 11:00:54 +0200

    removed sequelize

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-// @ts-expect-error eslint-disable-next-line
-import db from '../models/index.js';
 import { Request, Response } from 'express';
 
 const host = process.env.HOST ?? '0.0.0.0';
@@ -19,20 +17,12 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/roles', async (req: Request, res: Response) => {
   try {
-    const roles = await db.Role.findAll();
-    res.json(roles);
+    res.send({ message: 'Not yet implemented' });
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch roles' });
   }
 });
 
-// Sync database
-db.sequelize.sync()
-  .then(() => {
-    console.log('Database synced !');
-    app.listen(port, host, () => {
-      console.log(`[ ready ] http://${host}:${port}`);
-    });
-  }).catch((err: any) => {
-  console.error('Unable to sync database:', err);
+app.listen(port, host, () => {
+  console.log(`[ ready ] http://${host}:${port}`);
 });
