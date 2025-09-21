a836c868cb39edd468e9f46d35af034296f0112c
Constant <constantsuchet@gmail.com>
2025-09-21 11:35:44 +0200

    regenerated

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,14 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
-import * as path from 'path';
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to backend!' });
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello API' });
 });
 
-const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+app.listen(port, host, () => {
+  console.log(`[ ready ] http://${host}:${port}`);
 });
-server.on('error', console.error);
